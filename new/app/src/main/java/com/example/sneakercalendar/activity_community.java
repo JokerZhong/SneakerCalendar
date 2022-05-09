2d6a986946264ca34012ca0a715b159e4b24bf47
Joker Zhong <jiahaozhong@mail.adelphi.edu>
2022-05-09 12:18:05 -0400

    new file:   __MACOSX/new/app/src/main/res/drawable/._new_snkrs.jpeg 	new file:   __MACOSX/new/app/src/main/res/drawable/._news.webp 	new file:   __MACOSX/new/app/src/main/res/drawable/._newshoes.webp 	new file:   __MACOSX/new/app/src/main/res/drawable/._storyicon.png 	modified:   my.db 	modified:   new/.idea/misc.xml 	modified:   new/app/src/main/java/com/example/sneakercalendar/activity_community.java 	renamed:    new/app/src/main/res/drawable/new_snkrs.jpg -> new/app/src/main/res/drawable/new_snkrs.jpeg 	new file:   new/app/src/main/res/drawable/news.webp 	new file:   new/app/src/main/res/drawable/newshoes.webp 	new file:   new/app/src/main/res/drawable/storyicon.png 	modified:   new/app/src/main/res/layout/activity_community.xml 	modified:   new/build.gradle 	modified:   new/gradle/wrapper/gradle-wrapper.properties

--- new/app/src/main/java/com/example/sneakercalendar/activity_community.java
+++ new/app/src/main/java/com/example/sneakercalendar/activity_community.java
@@ -2,34 +2,14 @@ package com.example.sneakercalendar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.w3c.dom.Comment;
-
-import java.util.List;
-import com.example.sneakercalendar.DataServer.DBOpenHelper;
-
 public class activity_community extends AppCompatActivity {
-
-    ImageView imgPost,imgUserPost,imgCurrentUser;
-    TextView txtPostDesc,txtPostDateName,txtPostTitle;
-    EditText editTextComment;
-    Button btnAddComment;
-    String PostKey;
-    RecyclerView RvComment;
-
-    List<Comment> listComment;
-    static String COMMENT_KEY = "Comment" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
